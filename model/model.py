b4d0d7f54307e9aef03d44505668d89c3764eb3e
AlessiaCicca <162203648+AlessiaCicca@users.noreply.github.com>
2024-06-24 15:50:37 +0200

    Update model.py

--- model/model.py
+++ model/model.py
@@ -1,3 +1,23 @@
+import networkx as nx
+
+from database.DAO import DAO
+
+
 class Model:
     def __init__(self):
-        pass
+        self.grafo=nx.Graph()
+        self._idMap = {}
+
+    def creaGrafo(self, ngoal):
+        self.nodi = DAO.getNodi(ngoal)
+        self.grafo.add_nodes_from(self.nodi)
+        #self.addEdges(*)
+        for v in self.nodi:
+            self._idMap[v.PlayerID] = v
+        return self.grafo
+
+    def getNumNodes(self):
+        return len(self.grafo.nodes)
+
+    def getNumEdges(self):
+        return len(self.grafo.edges)
