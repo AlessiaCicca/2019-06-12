5ffc289f0d284f33b340b382bd917fce0a44b508
AlessiaCicca <162203648+AlessiaCicca@users.noreply.github.com>
2024-06-24 15:50:58 +0200

    Update DAO.py

--- database/DAO.py
+++ database/DAO.py
@@ -1,6 +1,29 @@
 from database.DB_connect import DBConnect
+from model.giocatore import Giocatore
 
 
 class DAO():
     def __init__(self):
         pass
+
+    @staticmethod
+    def getNodi(minGoal):
+        conn = DBConnect.get_connection()
+
+        result = []
+
+        cursor = conn.cursor(dictionary=True)
+        query = """select p.*
+from players p ,actions a 
+where p.PlayerID =a.PlayerID 
+group by p.PlayerID 
+having avg(a.Goals)>%s """
+
+        cursor.execute(query,(minGoal,))
+
+        for row in cursor:
+            result.append(Giocatore(**row))
+
+        cursor.close()
+        conn.close()
+        return result
